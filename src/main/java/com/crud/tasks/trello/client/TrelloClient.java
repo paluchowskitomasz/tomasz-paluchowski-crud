2d5c8d2bab59ba9922d49457144f6bf44a251f4d
paluchowskitomasz <paluchowski.tomasz@gmail.com>
2020-05-02 18:13:18 +0200

    22.2. Rest Template Zadanie: Ułożenie kodu

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -11,6 +11,7 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 
 @Component
@@ -41,16 +42,13 @@ public class TrelloClient {
          return url;
     }
 
-    public List<TrelloBoardDto> getTrelloBoards() {
+    public Optional<List<TrelloBoardDto>> getTrelloBoards() {
 
         String url = getURL().toString();
-
         TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
 
-        if (boardsResponse != null) {
-            return Arrays.asList(boardsResponse);
-        }
-        return new ArrayList<>();
+        return Optional.ofNullable(Arrays.asList(boardsResponse));
+
     }
 
 }
